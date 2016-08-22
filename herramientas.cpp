a80e87fff0de7b36f6f82050394aa52500bc5dac
Sebastian <jsleong@unal.edu.co>
2016-08-22 00:03:37 -0500

    funciona, nada importante

--- herramientas.cpp
+++ herramientas.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <time.h>
 using namespace std;
 const int N=10;
 
@@ -9,14 +10,24 @@ int main()
 {
 int Vect[2*N] = {0};
 //debe haber un for para el time
-for (int i=0;i<=N;i++) //TRATAR DE INICIALIZAR FUERZAS
+for (int i=1;i<=N;i++) //TRATAR DE INICIALIZAR FUERZAS VAMOS A COLOCARLAS EN LAS ENTRADAS IMPARES
 	{
 	Vect[2*i-1] = 1;
+	Vect[2*i]=i;
 	}
-	
-	for(int i=0;i<2*N;i++)
+
+for(int time=1;time<3;time++)
 	{
-	printf("Entrada %d \t Contenido %d \n",i+1,Vect[i]);
+		//DE ALGUNA MANERA SACAR CAD UNIDAD Y SACAR SU PROBABILIDAD DE SEPARARSE
+		for(int unit=1;unit<=N;unit++)
+		{
+		
+		}
+	}
+
+	for(int i=0;i<N;i++) //IMPRIME VALOR DE LA FUERZA Y EN QUE UNIDAD
+	{
+	printf(" Unidad [%d] \t Fuerza [%d] \n",1+Vect[2*i],Vect[2*i+1]);
 	}
 return 0;
 }
